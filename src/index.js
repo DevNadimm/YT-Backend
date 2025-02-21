1bbe5869eb2e5476a9d431e059c2881e76fb023e
DevNadimm <nadimchowdhury87@gmail.com>
2025-02-21 09:18:04 +0600

    refactor: setup Express app and enhance MongoDB connection handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import express from "express";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: './env'
 })
-const app = express();
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.error("ERROR ", error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.error("MongoDB connection failed! ", err);
+})
 
 
 
