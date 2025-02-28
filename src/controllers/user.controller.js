9bf730784c321694a6be1d7316a1ed276e8492af
DevNadimm <nadimchowdhury87@gmail.com>
2025-02-28 11:44:19 +0600

    feat: implement user login functionality with token generation and error handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,9 +4,27 @@ import { ApiResponse } from "../utils/api_response.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return {
+            accessToken,
+            refreshToken,
+        }
+    } catch (error) {
+        throw new ApiError(500, "An error occurred while generating refresh and access tokens.");
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     
-    // Steps
+    // TODOS:
     // get user details from frontend
     // validation - not empty
     // check if user already exists: username, email
@@ -18,7 +36,6 @@ const registerUser = asyncHandler(async (req, res) => {
     // return response
 
     const { username, fullName, email, password } = req.body;
-    //console.log("Request Body:", req.body);
 
     if (!fullName?.trim() || !username?.trim() || !email?.trim() || !password?.trim()) {
         throw new ApiError(400, "All fields are required");
@@ -39,16 +56,16 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Avatar file is required");
     };
 
+    if(!avatar) {
+        throw new ApiError(500, "Failed to upload avatar file");
+    };
+
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     
     let coverImage = "";
     if(coverImageLocalPath){
         coverImage = await uploadOnCloudinary(coverImageLocalPath);
     }
-
-    if(!avatar) {
-        throw new ApiError(500, "Failed to upload avatar file");
-    };
 
     const user = await User.create({
         fullName,
@@ -70,6 +87,65 @@ const registerUser = asyncHandler(async (req, res) => {
     return res.status(201).json(
         new ApiResponse(201, createdUser, "User registered successfully")
     )
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+
+    // TODOS: 
+    // req body -> data
+    // username, email validation
+    // find the user
+    // password check
+    // access and refresh token generate
+    // send cookies or response
+
+    const { email, username, password } = req.body;
+
+    if (!(username || email)) {
+        throw new ApiError(400, "Either username or email is required.");
+    }
+
+    if(!password) {
+        throw new ApiError(400, "Password is required.");
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user) {
+        throw new ApiError(404, "The requested user does not exist.");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid) {
+        throw new ApiError(401, "Password is incorrect.");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id)
+    .select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    };
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200, 
+            { user: loggedInUser, accessToken, refreshToken }, 
+            "User logged in successfully"
+        )
+    );
 })
 
-export { registerUser };
+export {
+    registerUser,
+    loginUser,
+};
